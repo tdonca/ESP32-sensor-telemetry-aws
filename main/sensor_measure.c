c16607494b2589df901b6bb6f8b4baae0a475552
Tudor Donca <tgdonca@gmail.com>
2021-11-30 15:40:20 -0800

    Refactored sensor code into separate functions for modularity

--- main/sensor_measure.c
+++ main/sensor_measure.c
@@ -13,11 +13,90 @@ static const char *TAG = "sensor-measure-i2c";
 
 #define SENSOR_HTU21D_ADDR 0x40
 #define SENSOR_TEMP_READ_COMMAND_NH 0xF3
+#define SENSOR_HUMID_READ_COMMAND_NH 0xF5
+#define SENSOR_SOFT_RESET_COMMAND 0xFE
 
-//TODO: break up main() into helper funcs
-int app_main(void) {
-    ESP_LOGI(TAG, "STARTING SENSOR READING APPLICATION");
+#define SENSOR_DELAY_RESET_MS 15
+#define SENSOR_DELAY_TEMP_MS 50
 
+/********************************************/
+
+static double compute_temperature(uint8_t msb, uint8_t lsb) {
+    uint16_t raw_temp = msb;
+    raw_temp = (raw_temp << 8); // move MSB into bits 15-8
+    raw_temp = raw_temp | (uint16_t)lsb; // add LSB into bits 7-0
+    raw_temp = raw_temp & 0xFFFC; // remove the last two status bits
+    ESP_LOGD(TAG, "Raw temp. data: 0x%x", raw_temp);
+
+    return (raw_temp * 175.72 / 65536.0) - 46.85;
+}
+
+static uint8_t compute_crc8(uint8_t msb, uint8_t lsb) {
+    uint32_t data_row = ((uint32_t)msb << 16) | ((uint32_t)lsb << 8); // [msb_lsb_00000000] 24bit
+    uint32_t divisor_poly = 0x131; // [100110001] 9-bit CRC8 polynomial X^8+X^5+X^4+1
+    divisor_poly <<= 15; // align 9bit divisor with the left side of 24bit row
+
+    // Shift the divisor along the data row until the divisor right-edge 
+    // reaches the data row right-edge. This operation "divides" the data 
+    // row by the divisor. Leaving the last 8 bits as the 
+    // "division remainder". This calculated remainder is the CRC value.
+    for (int i = 0; i < 16; i++) {
+        
+        // Perform XOR if the left-most 1bit of the divisor aligns with a 1bit in the data row
+        // The initial left-most 1bit of the 24bit aligned divisor is hardcoded as 23 
+        // The offset calculation tracks this index as the divisor shifts along the data row
+        if ((data_row >> (23 - i)) == 1) {
+            data_row = data_row ^ divisor_poly;
+        }
+
+        divisor_poly >>= 1;
+    }
+
+    // Return the remaining 8 least significant bits as the computed CRC value
+    return (data_row & 0xFF);
+}
+
+static esp_err_t sensor_write_command_i2c(int command) {
+    i2c_cmd_handle_t cmd_write = i2c_cmd_link_create();
+    i2c_master_start(cmd_write);
+    i2c_master_write_byte(cmd_write, (SENSOR_HTU21D_ADDR << 1 | I2C_MASTER_WRITE), true);
+    i2c_master_write_byte(cmd_write, command, true);
+    i2c_master_stop(cmd_write);
+    esp_err_t result = i2c_master_cmd_begin(I2C_CONTROLLER_NUM, cmd_write, I2C_MASTER_TIMEOUT / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd_write);
+    
+    return result;
+}
+
+static esp_err_t sensor_read_command_i2c(uint8_t *data_msb, uint8_t *data_lsb) {
+    uint8_t data_crc = 0;
+    i2c_cmd_handle_t cmd_read = i2c_cmd_link_create();
+    i2c_master_start(cmd_read);
+    i2c_master_write_byte(cmd_read, (SENSOR_HTU21D_ADDR << 1 | I2C_MASTER_READ), true);
+    i2c_master_read_byte(cmd_read, data_msb, I2C_MASTER_ACK);
+    i2c_master_read_byte(cmd_read, data_lsb, I2C_MASTER_ACK);
+    i2c_master_read_byte(cmd_read, &data_crc, I2C_MASTER_NACK);
+    i2c_master_stop(cmd_read);
+    esp_err_t result = i2c_master_cmd_begin(I2C_CONTROLLER_NUM, cmd_read, I2C_MASTER_TIMEOUT / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(cmd_read);
+
+    // Perform CRC on the sensor data
+    if (result == ESP_OK) {
+        uint8_t computed_crc = compute_crc8(*data_msb, *data_lsb);
+        if (data_crc != computed_crc) {
+            ESP_LOGE(TAG, "Sensor data CRC failed due to mismatch.");
+            ESP_LOGD(TAG, "Calculated CRC: 0x%x sensor provided CRC: 0x%x", computed_crc, data_crc);
+            result = ESP_FAIL;            
+        }
+    }
+
+    return result;
+}
+
+/********************************************/
+
+esp_err_t sensor_init(void) {
+    
     // Configure the I2C Driver
     i2c_config_t config = {};
     config.mode = I2C_MODE_MASTER;
@@ -30,8 +109,59 @@ int app_main(void) {
     ESP_LOGI(TAG, "CONFIGURED I2C PARAMS");
 
     // Install the I2C driver
-    ESP_ERROR_CHECK(i2c_driver_install(I2C_CONTROLLER_NUM, config.mode, 0, 0, 0));
+    esp_err_t result = i2c_driver_install(I2C_CONTROLLER_NUM, config.mode, 0, 0, 0);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG, "I2C Driver install failed. Reason: %s", esp_err_to_name(result));
+        return result;
+    }
     ESP_LOGI(TAG, "INSTALLED I2C DRIVER");
+
+    // Reset the sensor and verify operation
+    //TODO: prevent infinite loop if the sensor is broken
+    ESP_LOGI(TAG, "PERFORMING SOFT RESET AND WAITING FOR SENSOR RESPONSE...");
+    do {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        result = sensor_write_command_i2c(SENSOR_SOFT_RESET_COMMAND);
+        vTaskDelay(SENSOR_DELAY_RESET_MS / portTICK_PERIOD_MS);
+    } while(result != ESP_OK);
+
+    return result;
+}
+
+esp_err_t sensor_read_temperature(double* result_temp) {
+    if (result_temp == NULL) {
+        return ESP_FAIL;
+    }
+
+    // Send a temperature reading request to the sensor
+    esp_err_t result = sensor_write_command_i2c(SENSOR_TEMP_READ_COMMAND_NH);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG, "Temperature reading I2C write command failed. Reason: %s", esp_err_to_name(result));
+        return result;
+    }
+
+    // Wait for sensor measurement
+    vTaskDelay(SENSOR_DELAY_TEMP_MS / portTICK_PERIOD_MS);
+
+    // Read the data from the sensor
+    uint8_t data_buf[2] = {0};
+    result = sensor_read_command_i2c(&data_buf[0], &data_buf[1]);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG, "Temperature reading I2C read command failed. Reason: %s", esp_err_to_name(result));
+        return result;
+    }
+
+    // Convert the raw data to a temperature value
+    *result_temp = compute_temperature(data_buf[0], data_buf[1]);
+
+    return ESP_OK;
+}
+
+/***************************************/
+
+int app_main(void) {
+    ESP_LOGI(TAG, "STARTING SENSOR READING APPLICATION");
+    ESP_ERROR_CHECK(sensor_init());
 
     // Loop for measuring temperature
     while (1) {
@@ -40,53 +170,10 @@ int app_main(void) {
         vTaskDelay(3000 / portTICK_PERIOD_MS);
         ESP_LOGI(TAG, "STARTED SENSOR READING LOOP");
         
-        // Command link to request a temp reading from the sensor
-        i2c_cmd_handle_t cmd_request = i2c_cmd_link_create();
-        i2c_master_start(cmd_request);
-        i2c_master_write_byte(cmd_request, (SENSOR_HTU21D_ADDR << 1 | I2C_MASTER_WRITE), true);
-        i2c_master_write_byte(cmd_request, SENSOR_TEMP_READ_COMMAND_NH, true);
-        i2c_master_stop(cmd_request);
-        esp_err_t result = i2c_master_cmd_begin(I2C_CONTROLLER_NUM, cmd_request, I2C_MASTER_TIMEOUT / portTICK_PERIOD_MS);
-        i2c_cmd_link_delete(cmd_request);
-        if (result != ESP_OK) {
-            ESP_LOGE(TAG, 
-                    "I2C Initial Request master_cmd_begin failed: %s. Skipping iteration...", 
-                    esp_err_to_name(result));
-            continue;
-        }
-
-        // Delay for sensor measurement (max 50ms)
-        vTaskDelay(50 / portTICK_PERIOD_MS);
-
-        // Command link to read the temp data from the sensor
-        uint8_t sensor_data[2] = {0};
-        uint8_t crc = 0;
-        i2c_cmd_handle_t cmd_read = i2c_cmd_link_create();
-        i2c_master_start(cmd_read);
-        i2c_master_write_byte(cmd_read, (SENSOR_HTU21D_ADDR << 1 | I2C_MASTER_READ), true);
-        i2c_master_read(cmd_read, sensor_data, 2, I2C_MASTER_ACK);
-        i2c_master_read_byte(cmd_read, &crc, I2C_MASTER_NACK);
-        i2c_master_stop(cmd_read);
-        result = i2c_master_cmd_begin(I2C_CONTROLLER_NUM, cmd_read, I2C_MASTER_TIMEOUT / portTICK_PERIOD_MS);
-        i2c_cmd_link_delete(cmd_read);
-        if (result != ESP_OK) {
-            ESP_LOGE(TAG, 
-                    "I2C Read Sensor Measurement command failed: %s. Skipping iteration...",
-                    esp_err_to_name(result));
-            continue;
-        }
-
-        // CRC check
-        //TODO: implement
-
-        // Raw data conversion
-        uint16_t raw_temp = sensor_data[0];
-        raw_temp = (raw_temp << 8); // move MSB into bits 15-8
-        raw_temp = raw_temp | (uint16_t)sensor_data[1]; // add LSB into bits 7-0
-        raw_temp = raw_temp & 0xFFFC; // remove the last two status bits
-        double temperature = (raw_temp * 175.72 / 65536.0) - 46.85;
-
-        // Print temp reading to monitor
-        ESP_LOGI(TAG, "Raw data: %d, Temperature Reading: %f C\n\n", raw_temp, temperature);
+        // Get temperature reading from the sensor
+        double temperature = 0;
+        if (ESP_OK == sensor_read_temperature(&temperature)) {
+            ESP_LOGI(TAG, "Temperature Reading: %.2f C\n", temperature);
+        }        
     }  
 }
